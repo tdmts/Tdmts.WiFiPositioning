b66dd65d9586b7f247ed4f7b44e8458f38117f0e
Tom Demets <tom_demets@outlook.com>
2017-01-10 19:26:30 +0100

    Initial version

---
+++ Tdmts.WiFiPositioning.BL/WiFiScanner.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Devices.WiFi;
+using Windows.UI.Xaml;
+
+namespace Tdmts.WiFiPositioning.BL
+{
+    public class WiFiScanner
+    {
+
+        private DispatcherTimer _dispatcherTimer = new DispatcherTimer();
+
+        private Dictionary<string, WiFiAdapter> _wiFiAdapters = new Dictionary<string, WiFiAdapter>();
+        private int _scanInterval = 0;
+
+        private Dictionary<string, double> _position = new Dictionary<string, double>();
+
+        public event EventHandler<Dictionary<string, double>> PositionUpdated;
+        
+        public WiFiScanner()
+        {
+            _dispatcherTimer.Interval = TimeSpan.FromMilliseconds(ScanInterval);
+            _dispatcherTimer.Tick += _dispatcherTimer_Tick;
+            _dispatcherTimer.Start();
+        }
+
+        private async void _dispatcherTimer_Tick(object sender, object e)
+        {
+            _dispatcherTimer.Stop();
+            if(WiFiAdapters.Count==0)
+            {
+                await GetWiFiAdaptersAsync();
+            }
+            await UpdatePositions();
+            _dispatcherTimer.Start();
+        }
+
+        public async Task<Dictionary<string, WiFiAdapter>> GetWiFiAdaptersAsync()
+        {
+            IReadOnlyList<WiFiAdapter> adapters = await WiFiAdapter.FindAllAdaptersAsync();
+            if(adapters.Count>0)
+            {
+                foreach(WiFiAdapter adapter in adapters)
+                {
+                    if(!WiFiAdapters.ContainsKey(adapter.NetworkAdapter.NetworkAdapterId.ToString()))
+                    {
+                        //https://msdn.microsoft.com/en-us/library/windows.networking.connectivity.networkadapter.ianainterfacetype.aspx
+                        if (adapter.NetworkAdapter.IanaInterfaceType == 71)
+                        {
+                            WiFiAdapters.Add(adapter.NetworkAdapter.NetworkAdapterId.ToString(), adapter);
+                        }
+                    }
+                }
+            }
+            return WiFiAdapters;
+        }
+        
+        public async Task<Dictionary<string, double>> UpdatePositions()
+        {
+            foreach (var wiFiAdapter in WiFiAdapters)
+            {
+                await wiFiAdapter.Value.ScanAsync();
+                IReadOnlyList<WiFiAvailableNetwork> availableNetworks = wiFiAdapter.Value.NetworkReport.AvailableNetworks;
+
+                foreach (WiFiAvailableNetwork availableNetwork in availableNetworks)
+                {
+                    if (Position.ContainsKey(availableNetwork.Ssid))
+                    {
+                        Position[availableNetwork.Ssid] = availableNetwork.NetworkRssiInDecibelMilliwatts;
+                        PositionUpdated(this, Position);
+                    }
+                    else
+                    {
+                        Position.Add(availableNetwork.Ssid, availableNetwork.NetworkRssiInDecibelMilliwatts);
+                    }
+                }
+            }
+            return Position;
+        }
+
+        public double GetDistanceFromRssiAndTxPowerOn1m(double rssi, int txPower)
+        {
+            /*
+             * RSSI = TxPower - 10 * n * lg(d)
+             * n = 2 (in free space)
+             * 
+             * d = 10 ^ ((TxPower - RSSI) / (10 * n))
+             */
+            return Math.Pow(10, ((double)txPower - rssi) / (10 * 2));
+        }
+
+        public Dictionary<string, double> Position
+        {
+            get
+            {
+                return _position;
+            }
+
+            set
+            {
+                _position = value;
+            }
+        }
+
+        public Dictionary<string, WiFiAdapter> WiFiAdapters
+        {
+            get
+            {
+                return _wiFiAdapters;
+            }
+
+            set
+            {
+                _wiFiAdapters = value;
+            }
+        }
+
+        public int ScanInterval
+        {
+            get
+            {
+                return _scanInterval;
+            }
+
+            set
+            {
+                if(value<5000)
+                {
+                    value = 5000;
+                }
+                _scanInterval = value;
+            }
+        }
+    }
+}
---
+++ Tdmts.WiFiPositioning.GUI/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Tdmts.WiFiPositioning.BL;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Tdmts.WiFiPositioning.GUI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        WiFiScanner _scanner = new WiFiScanner();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _scanner.PositionUpdated += _scanner_PositionUpdated;
+        }
+
+        private void _scanner_PositionUpdated(object sender, Dictionary<string, double> e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var position in e)
+            {
+                double distance = Math.Round(_scanner.GetDistanceFromRssiAndTxPowerOn1m(position.Value, -29),2);
+
+                string s = string.Format("{0} : {1} : approximately {2}m away from you.", position.Key, position.Value, distance);
+                sb.AppendLine(s);
+            }
+
+            txtAccessPoints.Text = sb.ToString();
+        }
+
+        private void btnScan_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
